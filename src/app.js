69bd034f3c5892986dcd7dec79d24d8b6d974a41
Nishika <nishikasasidharan.7@gmail.com>
2021-10-19 14:51:21 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,89 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
+const app= express()
+const publicDirectory=path.join(__dirname,'../public')
+const viewTemplates=path.join(__dirname,'../Templates/views')
+const viewPartial=path.join(__dirname,'../Templates/partials')
+app.set('view engine','hbs')
+app.set('views',viewTemplates)
+app.use(express.static(publicDirectory))
+hbs.registerPartials(viewPartial)
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'nishika' 
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        name:'nishika',
+        title:'About'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        content:'Hai this small paragraph for the page'
+    })
+})
+
+    
+
+
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+   
+       geocode(req.query.address,(error,{lattitude,longitude,location}={})=>{
+           if(error){
+               return res.send({error})
+           }
+           forecast(lattitude,longitude,(error,forecastData)=>{
+
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address:req.query.address
+            })
+
+           })
+       })
+})
+
+
+app.get('/help/*',(req,res)=>{
+    res.render('404page',{
+        title:'404',
+        name:'Nishika',
+        errorMessage:'help page is not found'
+    })
+})
+app.get('/*',(req,res)=>{
+    res.render('404page',{
+        title:'404',
+        name:'Nishika',
+        errorMessage:'Page is not found'
+    })
+})
+
+app.listen(3000,()=>
+{
+    console.log('Server is upon the port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request=require('request')
+const forecast=(lattitude,longitude,callback)=>{
+    const url='http://api.weatherstack.com/current?access_key=207ba324dbc8289cb2fe460cfb00cbe1&query='+lattitude+','+longitude+'&units=m'
+    request({url,json:true},(error,{body})=>{
+            if(error){
+                callback('Unable to connect weather service',undefined)
+            }
+            else if(body.error){
+        
+                callback('please provide valid address',undefined)
+            }
+            else{
+            callback(undefined,body.current.weather_descriptions[0]+'. it is currently'+body.current.temperature+'degrees out,there is'+ body.current.precip+'% chance of rain')
+            }
+        })
+
+}
+module.exports=forecast
